fbebd6719208e0f0ca0871990c216ee4c4a4729c
kangjaspreet <jaspreetkang@g.ucla.edu>
2022-03-03 18:35:19 -0800

    Basic charting

--- B - Simple Charts.R
+++ B - Simple Charts.R
@@ -1,15 +1,20 @@
-add comments....
+# the library() function loads needed packages for the current session
+#  these packages must have already been installed 
+#  there are several ways to do this including:  install.packages("readr") etc.
 
+library(dplyr) # core "tidyverse" package for data manipulation
+library(ggplot2) # data visualization package
+library(readr) # tools for reading all sorts of data into R
+library(tidyr) # "tidyverse" package mainly used to convert between different data formats (long vs wide)
 
-library(dplyr)
-library(ggplot2)
-library(readr)
-library(tidyr)
-
+# URL for California COVID-19 data on the CHHS Open Data Portal
 covid_re_url <- "https://data.chhs.ca.gov/dataset/f88f9d7f-635d-4334-9dac-4ce773afe4e5/resource/b500dae2-9e58-428e-b125-82c7e9b07abb/download/covid19demographicratecumulative.csv"
 
+# Read the Covid data directly from the Open Data Portal, and filter on race/ethnicity COVID data in California
 covid_re     <- read_csv(covid_re_url) %>%  
-  filter(county == "California", demographic_set == "race_ethnicity")
+  filter(county == "California", demographic_set == "race_ethnicity") 
+
+
 
 
 
@@ -19,21 +24,54 @@ maybe a scatterplot?
   
 
 
+# Filter on cases
+case_data <- covid_re %>% 
+  filter(metric == "cases", 
+         !demographic_set_category %in% c("Other", "Unknown"))
 
-temp_data <- covid_re %>% filter(metric == "cases")
-
-
-barplot(temp_data$metric_value_per_100k)
-barplot(temp_data$metric_value_per_100k, names.arg = temp_data$demographic_set_category)
-
-
-#  J fix labels; not other ... unknown...
+# Produce simple bar chart of case rate per 100k population by race/ethnicity
+barplot(case_data$metric_value_per_100k)
 
 
+# Let's add race/ethnicity labels
+barplot(case_data$metric_value_per_100k, 
+        names.arg = case_data$demographic_set_category)
 
-# same chart with gglot
-# same chart with gglot  horizontal ...
-# same chart + ???
+# Now let's add titles for each axis, and the plot
+barplot(case_data$metric_value_per_100k, 
+        names.arg = case_data$demographic_set_category, 
+        main = "COVID-19 Case Rate per 100k Population by Race/Ethnicity in CA", 
+        xlab = "Race/Ethnicity", 
+        ylab = "Case Rate per 100k Population")
+
+
+# The charts above were created using base R commands.
+# Add note here about ggplot2
+
+# Simple bar chart of case rate per 100k population by race/ethnicity, using ggplot2
+ggplot(data = case_data, aes(x = demographic_set_category, y = metric_value_per_100k)) +
+  geom_bar(stat = 'identity') +
+  labs(title = "COVID-19 Case Rate per 100k Population by Race/Ethnicity in CA", 
+       x = "Race/Ethnicity", 
+       y = "Case Rate per 100k Population")
+
+
+# Same chart, but as a horizontal bar chart
+ggplot(data = case_data, aes(x = demographic_set_category, y = metric_value_per_100k)) +
+  geom_bar(stat = 'identity') +
+  coord_flip() + # This rotates the chart
+  labs(title = "COVID-19 Case Rate per 100k Population by Race/Ethnicity in CA", 
+       x = "Race/Ethnicity", 
+       y = "Case Rate per 100k Population")
+
+# Add # of case as text labels inside each bar
+ggplot(data = case_data, aes(x = demographic_set_category, y = metric_value_per_100k)) +
+  geom_bar(stat = 'identity') +
+  geom_text(aes(label = paste0("N = ", metric_value)), vjust = 1, color = 'white') +
+  #coord_flip() + # This rotates the chart
+  labs(title = "COVID-19 Case Rate per 100k Population by Race/Ethnicity in CA", 
+       x = "Race/Ethnicity", 
+       y = "Case Rate per 100k Population")
 
 
 
